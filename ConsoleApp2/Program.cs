e5e38ac2416c7510d0e381c46a2179c034d52c4d
qiyang7 <95297809+qiyang7@users.noreply.github.com>
2023-05-27 18:06:12 +0800

    WWrong loa

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello,Siuuu!"); Console.WriteLine("Hello,Siuuu!"); Console.WriteLine("Hello,Siuuu!"); Console.WriteLine("Hello,Siuuu!"); Console.WriteLine("Hello,Siuuu!"); Console.WriteLine("Hello,Siuuu!");
+Console.WriteLine("Hello,Siuuu!");
